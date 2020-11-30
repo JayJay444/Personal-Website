58bf4a56910ee01845f9a3c33a37277b56767829
Jakub <kubakmag44@gmail.com>
2020-11-30 13:18:08 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("link-about").addEventListener("click", function(){
+  window.scrollTo(0, 40);
+});
+document.getElementById("link-techno").addEventListener("click", function(){
+  window.scrollTo(0, 840);
+});
+document.getElementById("link-contact").addEventListener("click", function(){
+  window.scrollTo(0, 1640);
+});
