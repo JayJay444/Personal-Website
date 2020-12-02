ad93326085ed3ddd0624af100b4604acc12d30d5
Jakub <kubakmag44@gmail.com>
2020-12-02 10:54:19 +0100

    add projects section and adjust scrolling with navbar-links

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-document.getElementById("link-about").addEventListener("click", function(){
+document.getElementById("link-about").addEventListener("click", function() {
   window.scrollTo(0, 40);
 });
-document.getElementById("link-techno").addEventListener("click", function(){
-  window.scrollTo(0, 840);
+document.getElementById("link-techno").addEventListener("click", function() {
+  if (window.innerWidth < 800) {
+    window.scrollTo(0, 840);
+  } else {
+    window.scrollTo(0, 440);
+  }
 });
-document.getElementById("link-contact").addEventListener("click", function(){
-  window.scrollTo(0, 1640);
+document.getElementById("link-contact").addEventListener("click", function() {
+  if (window.innerWidth < 800) {
+    window.scrollTo(0, 1640);
+  } else {
+    window.scrollTo(0, 840);
+  }
+});
+document.getElementById("link-projects").addEventListener("click", function() {
+  if (window.innerWidth < 800) {
+    window.scrollTo(0, 2440);
+  } else {
+    window.scrollTo(0, 1240);
+  }
 });
